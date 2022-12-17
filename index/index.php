7f086c55f2071ad00d7f5743bc1b9d04624588ee
Jose David <jose7312000@gmail.com>
2022-12-16 23:34:01 -0500

    Haciendo ajustes

--- index/index.php
+++ index/index.php
@@ -51,20 +51,67 @@ echo "El promedio entre los 3 puntos es: " . $prom;
 //No hay forma de obtener un palindromo modificando 
 //un solo carácter, es necesario modificar al menos dos. Por ejemplo la f por una b y la g por una a.
 
+
 function IsCasiPalindromo($palabra)
 {
-    //Se crea la variable booleana
+    //Se crea la variable booleana para saber si se cumple la condicion y mostrar el mensaje esperado
     $palindromo = false;
 
-    //Se emplea un condicional utilizando el metodo strrev que nos sirve para invertir un string
-    if (strrev($palabra) === $palabra) {
+    // Convertimos la palabra a minúsculas para evitar problemas
+    // con mayúsculas y minúsculas
+    $palabra = strtolower($palabra);
 
-        //Si la condicion se cumple se cambia a true la variable palindromo de lo contrario se imprime en el valor inicial
+    // Longitud de la palabra
+    $longitud = strlen($palabra);
+
+    // Inicializamos el contador de diferencias
+    $diferencias = 0;
+
+    // Recorremos la palabra comparando cada par de caracteres
+    // en posiciones opuestas
+    for ($i = 0; $i < $longitud / 2; $i++) {
+        if ($palabra[$i] != $palabra[$longitud - $i - 1]) {
+            $diferencias++;
+        }
+    }
+
+    // Si hay una o cero diferencias, retornamos true
+    // (es un palíndromo o casi palíndromo)
+    if ($diferencias <= 1) {
         $palindromo = true;
         var_dump((bool) $palindromo); 
-    } else {
-        var_dump((bool) $palindromo); 
+    }else{
+        
+    // En caso contrario, retornamos false
+    var_dump((bool) $palindromo); 
+    }
+
+}
+
+echo IsCasiPalindromo("Ojo");
+
+
+
+//Escriba una funcion que recibe un array de enteros positivos con valores entre 1 y 5,000 y la longitud del array , y retorne el 
+//numero que mas veces aparece en el array. 
+//En el caso de que dos o mas numeros aparezcan la misma cantidad de veces la funcion retorna el menor numero entre aquellos que mas veces aparecen en el array.
+//Ejemplos:NumMasPopular([34,31,34,77,82],5) → 34 //es el unico que aparece 2 vecesNumMasPopular([22,101,102,101,102,525,88],7)
+// → 101 // 101 y 102 aparece dos veces cada una pero 101<102NumMasPopular([66],1) → 66 
+//Es el unico numeroNumMasPopular([14,14,2342,2342,2342],5) → 2342 //aparece 3 veces mientras que 14 solo aparece dos veces.
+
+function numberRepeated($numbers, $length)
+{
+
+    //Validaciones
+
+    //Verificando que el array tenga numeros positivos y enteros
+
+    //Validando que esten entre 1 y 5,000
+
+    //Verificando que el numero de valores corresponda a la longitud ingresada
+    if (count($numbers) != $length) {
+        echo "La longitud que ingresó, no corresponde al numero de valores vuelta a intentarlo!";
     }
 }
 
-IsCasiPalindromo("Nombre");
+numberRepeated([10, 20, 45], 8);
